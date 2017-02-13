8f6bb47e27b6adb8231acaa6ad15c7600541f337
JavidPack <javidpack@gmail.com>
2017-02-12 18:21:48 -0700

    travis 15

--- AutoReleaseFromModBrowser.sh
+++ AutoReleaseFromModBrowser.sh
@@ -18,6 +18,7 @@ echo "git version is $gitVersion"
 
 if [ "$version" = "$gitVersion" ]; then 
 	echo "Version does match, no need to push release";
+	export NEEDDEPLOY=no
 	exit 1;
 else 
 	echo "Version does not match, need to push release";
@@ -30,6 +31,7 @@ else
 	echo "3";
 	git push --quiet https://$GH_REPO_TOKEN@github.com/JavidPack/BossChecklist $version > /dev/null 2>&1
 	echo "4";
+	export NEEDDEPLOY=yes
 	exit 0;
 fi
 echo "Travis Script End"
