eaed24613446801beffbf61040a43107f954c958
SheepishShepherd <cameronscottmcdougal@gmail.com>
2020-07-20 02:44:19 -0400

    Loot is now checked if item is crafted

--- ItemAssist.cs
+++ ItemAssist.cs
@@ -33,5 +33,30 @@ namespace BossChecklist
 			}
 			return base.OnPickup(item, player);
 		}
+
+		public override void OnCraft(Item item, Recipe recipe) {
+			if (Main.netMode != NetmodeID.Server) {
+				Player player = Main.LocalPlayer;
+				// Loot and Collections Updating
+				List<BossInfo> BossList = BossChecklist.bossTracker.SortedBosses;
+				PlayerAssist modplayer = player.GetModPlayer<PlayerAssist>();
+				for (int i = 0; i < BossList.Count; i++) {
+					int BossIndex = modplayer.BossTrophies.FindIndex(boss => boss.bossName == BossList[i].Key);
+					if (BossIndex == -1) continue;
+					// Loot Collections
+					if (BossList[i].loot.Contains(item.type)) {
+						if (modplayer.BossTrophies[i].loot.All(x => x.Type != item.type)) {
+							modplayer.BossTrophies[i].loot.Add(new ItemDefinition(item.type));
+						}
+					}
+					// Boss Collections
+					if (BossList[i].collection.Contains(item.type)) {
+						if (modplayer.BossTrophies[i].collectibles.All(x => x.Type != item.type)) {
+							modplayer.BossTrophies[i].collectibles.Add(new ItemDefinition(item.type));
+						}
+					}
+				}
+			}
+		}
 	}
 }
